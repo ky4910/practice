1c25d9f792f0e92ef4ac5772dd09017763e84a1a
kimber <kimber@kimber-System.viatech.com.cn>
2017-09-30 14:46:09 +0800

    add binary convert function

--- C/binary.c
+++ C/binary.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void decToBin(int num, char *buffer);
 
 int main()
 {
     int i = 0;
     int bit_1 = 0, bit_16 = 0;
     int num[16] = {1,0,0,0,  0,1,0,0,  1,1,1,0,  0,0,0,0};
+	int number = 10;
+	char str[10] = {'\0'};
 
     for (i = 15; i >= 0; i--)
     {
@@ -23,11 +28,28 @@ int main()
 	printf("result is 0x%x\n", bit_16);
 	printf("result is %d\n", bit_16);
 
+	decToBin(number, str);
+
+	printf("number %d => %s\n", number, str);
+	
 	//判断一个数是否为2的幂
 	/*
 		if (x&(x-1) == 0)
 			it is MI of 2
 	*/
     return 0;
+}
+
+void decToBin(int num, char *buffer)
+{
+	int i = 0;
+	int tmp = 0;
+
+	for (i=7; i>=0; i--)
+	{
+		tmp = num%2;
+		buffer[i] = tmp + '0';
+		num /= 2;
+	}
 }
 
